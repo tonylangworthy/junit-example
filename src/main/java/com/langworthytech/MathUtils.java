9394a0eaf5242a1a4d6466dc54b1b51e19ba0e6d
Tony Langworthy <tony.langworthy@hotmail.com>
2020-03-31 15:04:17 -0500

    Created first example unit test

---
+++ src/main/java/com/langworthytech/MathUtils.java
@@ -0,0 +1,8 @@
+package com.langworthytech;
+
+public class MathUtils {
+
+    public int add(int first, int second) {
+        return first + second;
+    }
+}
---
+++ src/test/java/com/langworthytech/MathUtilsTest.java
@@ -0,0 +1,18 @@
+package com.langworthytech;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilsTest {
+
+    @Test
+    public void test() {
+        MathUtils mathUtils = new MathUtils();
+
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+
+        assertEquals(expected, actual);
+    }
+}
