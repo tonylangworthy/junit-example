bf0bb102b49362aaf3a4e6768221415cdc5f96b7
Tony Langworthy <tony.langworthy@hotmail.com>
2020-03-31 20:15:47 -0500

    Added an assertThrows test

--- src/main/java/com/langworthytech/MathUtils.java
+++ src/main/java/com/langworthytech/MathUtils.java
@@ -2,11 +2,15 @@ package com.langworthytech;
 
 public class MathUtils {
 
-    public int add(int first, int second) {
-        return first + second;
+    public int add(int a, int b) {
+        return a + b;
     }
 
     public double computeCircleArea(double radius) {
         return Math.PI * radius * radius;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
     }
 }
--- src/test/java/com/langworthytech/MathUtilsTest.java
+++ src/test/java/com/langworthytech/MathUtilsTest.java
@@ -1,6 +1,6 @@
 package com.langworthytech;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
@@ -12,13 +12,22 @@ public class MathUtilsTest {
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
-        Assertions.assertEquals(expected, actual, "Method should add 2 integers");
+        assertEquals(expected, actual, "Method should add 2 integers");
+    }
+
+    @Test
+    public void testDivide() {
+        MathUtils mathUtils = new MathUtils();
+
+        assertThrows(ArithmeticException.class,
+                () -> mathUtils.divide(1, 0),
+                "Divide by zero should throw exception");
     }
 
     @Test
     public void testComputeCircleRadius() {
         MathUtils mathUtils = new MathUtils();
-        Assertions.assertEquals(314.1592653589793,
+        assertEquals(314.1592653589793,
                 mathUtils.computeCircleArea(10), "Should return right circle area");
     }
 }
