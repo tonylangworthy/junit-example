077dae64f27fb566a5a920e0606d67032a110bda
Tony Langworthy <tony.langworthy@hotmail.com>
2020-03-31 19:59:25 -0500

    Added a method the TDD way

--- src/main/java/com/langworthytech/MathUtils.java
+++ src/main/java/com/langworthytech/MathUtils.java
@@ -5,4 +5,8 @@ public class MathUtils {
     public int add(int first, int second) {
         return first + second;
     }
+
+    public double computeCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
 }
--- src/test/java/com/langworthytech/MathUtilsTest.java
+++ src/test/java/com/langworthytech/MathUtilsTest.java
@@ -1,18 +1,24 @@
 package com.langworthytech;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MathUtilsTest {
 
     @Test
-    public void test() {
+    public void testAdd() {
         MathUtils mathUtils = new MathUtils();
 
         int expected = 2;
         int actual = mathUtils.add(1, 1);
 
-        assertEquals(expected, actual, "The add method should add 2 numbers");
+        Assertions.assertEquals(expected, actual, "Method should add 2 integers");
+    }
+
+    @Test
+    public void testComputeCircleRadius() {
+        MathUtils mathUtils = new MathUtils();
+        Assertions.assertEquals(314.1592653589793,
+                mathUtils.computeCircleArea(10), "Should return right circle area");
     }
 }
